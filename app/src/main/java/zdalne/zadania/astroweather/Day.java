1762e192521cfccad03a03de455755183dbbf9f2
MavosV8 <lukaszpasi@gmail.com>
2022-02-17 01:01:11 +0100

    added logic to night and day fragments

--- app/src/main/java/zdalne/zadania/astroweather/Day.java
+++ app/src/main/java/zdalne/zadania/astroweather/Day.java
@@ -1,5 +1,6 @@
 package zdalne.zadania.astroweather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.astrocalculator.AstroCalculator;
+import com.astrocalculator.AstroDateTime;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +24,153 @@ import android.view.ViewGroup;
  */
 public class Day extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_PARAM1 = "longitude";
+    private static final String ARG_PARAM2 = "latitude";
+    private static final String ARG_PARAM3 = "refresh";
+
+    private AstroCalculator.SunInfo sunInfo;
+    private AstroDateTime astroDateTime;
+    private AstroCalculator.Location astroLocation;
+    private LocalDateTime localDateTime;
+
+    private TextView sunsetTime;
+    private TextView sunsetAzimuth;
+    private TextView sunriseTime;
+    private TextView sunriseAzimuth;
+    private TextView civilDawnTime;
+    private TextView civilDuskTime;
+
+
+    private TextView currentTimeDay;
+    private TextView longitudeDay;
+    private TextView latitudeDay;
+    private Thread sunUpdater;
+    private Thread phoneTimeUpdater;
+    private boolean exit = false;
+
+
+    private View rootView;
+    private String longitude;
+    private String latitude;
+    private int refresh;
 
     public Day() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Day.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Day newInstance(String param1, String param2) {
+
+    public static Day newInstance(String longi, String lati, int refresh) {
         Day fragment = new Day();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, longi);
+        args.putString(ARG_PARAM2, lati);
+        args.putInt(ARG_PARAM3, refresh);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    private void setupMoonUpdater(){
+        sunUpdater = new Thread(() -> {
+            while(!exit){
+                setSun();
+                try {
+                    Thread.sleep(refresh);
+                } catch (InterruptedException e) {
+                    exit = true;
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void setupPhoneTimeUpdater(){
+        phoneTimeUpdater = new Thread(() -> {
+            while(true){
+                setPhoneTime();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    exit = true;
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        setSun();
+        setPhoneTime();
+        setupMoonUpdater();
+        setupPhoneTimeUpdater();
+        sunUpdater.start();
+        phoneTimeUpdater.start();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            longitude = getArguments().getString(ARG_PARAM1);
+            latitude = getArguments().getString(ARG_PARAM2);
+            refresh = getArguments().getInt(ARG_PARAM3);
         }
+        sunsetTime = rootView.findViewById(R.id.sunsetTime);
+        sunsetAzimuth = rootView.findViewById(R.id.sunsetAzimuth);
+        sunriseTime = rootView.findViewById(R.id.sunriseTime);
+        sunriseAzimuth = rootView.findViewById(R.id.sunriseAzimuth);
+        civilDawnTime = rootView.findViewById(R.id.civilDawnTime);
+        civilDuskTime = rootView.findViewById(R.id.civilDuskTime);
+
+        currentTimeDay = rootView.findViewById(R.id.currentTimeDay);
+        longitudeDay = rootView.findViewById(R.id.longitudeDay);
+        latitudeDay = rootView.findViewById(R.id.latitudeDay);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_day, container, false);
+        rootView = inflater.inflate(R.layout.fragment_day, container, false);
+        return rootView;
     }
+
+    public void setSunInfo(){
+        localDateTime = LocalDateTime.now();
+        astroDateTime = new AstroDateTime(localDateTime.getYear(), localDateTime.getMonthValue(), localDateTime.getDayOfMonth(), localDateTime.getHour(), localDateTime.getMinute(), localDateTime.getSecond(), 1, TimeZone.getDefault().inDaylightTime( new Date()));
+        astroLocation = new AstroCalculator.Location(Double.parseDouble(longitude),Double.parseDouble(latitude));
+        AstroCalculator astroCalculator = new AstroCalculator(astroDateTime,astroLocation);
+        sunInfo = astroCalculator.getSunInfo();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setPhoneTime(){
+        localDateTime = LocalDateTime.now();
+        currentTimeDay.setText(localDateTime.getHour() + " " + localDateTime.getMinute());
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setSun(){
+        setSunInfo();
+        sunsetTime.setText(sunInfo.getSunset().getHour() + ":" + sunInfo.getSunset().getMinute());
+        sunriseTime.setText(sunInfo.getSunrise().getHour() + ":" + sunInfo.getSunrise().getMinute());
+        sunriseAzimuth.setText(String.valueOf(sunInfo.getAzimuthRise()));
+        sunsetAzimuth.setText(String.valueOf(sunInfo.getAzimuthSet()));
+        civilDawnTime.setText(sunInfo.getTwilightMorning().getHour() + ":" + sunInfo.getTwilightMorning().getMinute());
+        civilDuskTime.setText(sunInfo.getTwilightEvening().getHour() + ":" + sunInfo.getTwilightEvening().getMinute());
+
+    }
+
+    public void forceUpdate(int refresh){
+        sunUpdater.interrupt();
+        this.refresh = refresh;
+        exit = false;
+        sunUpdater.start();
+
+
+    }
+
+
 }
--- app/src/main/java/zdalne/zadania/astroweather/Night.java
+++ app/src/main/java/zdalne/zadania/astroweather/Night.java
@@ -1,5 +1,6 @@
 package zdalne.zadania.astroweather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,156 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Night#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.astrocalculator.AstroCalculator;
+import com.astrocalculator.AstroCalculator.*;
+import com.astrocalculator.AstroDateTime;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Night extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private MoonInfo moonInfo;
+    private AstroDateTime astroDateTime;
+    private Location astroLocation;
+    private LocalDateTime localDateTime;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_PARAM1 = "longitude";
+    private static final String ARG_PARAM2 = "latitude";
+    private static final String ARG_PARAM3 = "refresh";
+    private TextView moonsetTime;
+    private TextView moonriseTime;
+    private TextView newMoonDate;
+    private TextView fullMoonDate;
+    private TextView moonPhase;
+    private TextView dayOfLunarMonth;
+    private TextView currentTimeNight;
+    private TextView longitudeNight;
+    private TextView latitudeNight;
+    private Thread moonUpdater;
+    private Thread phoneTimeUpdater;
+    private boolean exit = false;
+    private View rootView;
+    private String longitude;
+    private String latitude;
+    private int refresh;
 
     public Night() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Night.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Night newInstance(String param1, String param2) {
+
+    public static Night newInstance(String longi, String lati, int refresh) {
         Night fragment = new Night();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, longi);
+        args.putString(ARG_PARAM2, lati);
+        args.putInt(ARG_PARAM3, refresh);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    private void setupMoonUpdater(){
+        moonUpdater = new Thread(() -> {
+            while(!exit){
+                setMoon();
+                try {
+                    Thread.sleep(refresh);
+                } catch (InterruptedException e) {
+                    exit = true;
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void setupPhoneTimeUpdater(){
+        phoneTimeUpdater = new Thread(() -> {
+            while(true){
+                setPhoneTime();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    exit = true;
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        setMoon();
+        setPhoneTime();
+        setupMoonUpdater();
+        setupPhoneTimeUpdater();
+        moonUpdater.start();
+        phoneTimeUpdater.start();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            longitude = getArguments().getString(ARG_PARAM1);
+            latitude = getArguments().getString(ARG_PARAM2);
+            refresh = getArguments().getInt(ARG_PARAM3);
         }
+        moonsetTime = rootView.findViewById(R.id.moonsetTime);
+        moonriseTime = rootView.findViewById(R.id.moonriseTime);
+        newMoonDate = rootView.findViewById(R.id.newMoonDate);
+        fullMoonDate = rootView.findViewById(R.id.fullMoonDate);
+        moonPhase = rootView.findViewById(R.id.moonPhase);
+        dayOfLunarMonth = rootView.findViewById(R.id.dayOfLunarMonth);
+        currentTimeNight = rootView.findViewById(R.id.currentTimeNight);
+        longitudeNight = rootView.findViewById(R.id.longitudeNight);
+        latitudeNight = rootView.findViewById(R.id.latitudeNight);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_night, container, false);
+        rootView = inflater.inflate(R.layout.fragment_night, container, false);
+        return rootView;
     }
+
+    public void setMoonInfo(){
+        localDateTime = LocalDateTime.now();
+        astroDateTime = new AstroDateTime(localDateTime.getYear(), localDateTime.getMonthValue(), localDateTime.getDayOfMonth(), localDateTime.getHour(), localDateTime.getMinute(), localDateTime.getSecond(), 1, TimeZone.getDefault().inDaylightTime( new Date()));
+        astroLocation = new Location(Double.parseDouble(longitude),Double.parseDouble(latitude));
+        AstroCalculator astroCalculator = new AstroCalculator(astroDateTime,astroLocation);
+        moonInfo = astroCalculator.getMoonInfo();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setPhoneTime(){
+        localDateTime = LocalDateTime.now();
+        currentTimeNight.setText(localDateTime.getHour() + " " + localDateTime.getMinute());
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setMoon(){
+        setMoonInfo();
+        moonsetTime.setText(moonInfo.getMoonset().getHour() + ":" + moonInfo.getMoonset().getMinute());
+        moonriseTime.setText(moonInfo.getMoonrise().getHour() + ":" + moonInfo.getMoonrise().getMinute());
+        newMoonDate.setText(moonInfo.getNextNewMoon().toString());
+        fullMoonDate.setText(moonInfo.getNextFullMoon().toString());
+        moonPhase.setText((int) moonInfo.getIllumination() + "%");
+        dayOfLunarMonth.setText((int) moonInfo.getAge());
+
+    }
+
+    public void forceUpdate(int refresh){
+        moonUpdater.interrupt();
+        this.refresh = refresh;
+        exit = false;
+        moonUpdater.start();
+
+    }
+
+
 }
